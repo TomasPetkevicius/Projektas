95c656a39cc118432c6e4653ec28ef77af5eabb2
TomasPetkevicius <48209088+TomasPetkevicius@users.noreply.github.com>
2019-04-10 11:58:19 +0300

    Update content.php

--- www_final/form/app/inc/content.php
+++ www_final/form/app/inc/content.php
@@ -1,13 +1,16 @@
-<main>
-            
-            <div class="container">
-            <form id="contact" action="index.php" method="post">
-                <h1>Susisiekti</h1>
+<main>            
+    <div class="center" align="center">    
+                <form action="index.php" method="post">
+                <h1>Kontaktai</h1>
+    <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Gedimino%20g.81%20Kaisiadorys%20&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Werbung: <a href="https://www.pureblack.de">pureblack.de</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
+                <br>
+                <h>el. paštas: info@tavosvaja.lt</h>
+                <br>
                 <h>Tel.:<a href="tel:+37067012811">8-670-12811</a><br>arba užpildykite žemiau esančią formą ir mes su jumis susisieksime patys!</h>
                 <p><input type="text" name="vardas" placeholder="Jūsų vardas" required autofocus></p>
                 <p><input type="email" name="email" placeholder="Jūsų el. pašto adresas" required></p>
                 <p><textarea placeholder="Jūsų žinutė..." name="message" required></textarea></p>
                 <p><button name="submit" type="submit" id="contact-submit">Siųsti</button></p>
-            </form> 
-            </div>
+            </form>             
+        </div>
         </main>
